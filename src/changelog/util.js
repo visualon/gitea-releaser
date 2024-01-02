57ca7866bca3a849f794b5211e29ee1028c540ba
Michael Kriese <michael.kriese@visualon.de>
2024-01-02 14:32:18 +0100

    feat: initial project

---
+++ src/changelog/util.js
@@ -0,0 +1,78 @@
+import conventionalChangelogPreset from 'conventional-changelog-conventionalcommits';
+import conventionalChangelogCore from 'conventional-changelog-core';
+
+/**
+ * @type {import('conventional-changelog-core').Options}
+ */
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+export const config = conventionalChangelogPreset({
+  types: [
+    {
+      type: 'feat',
+      section: 'Features',
+    },
+    {
+      type: 'feature',
+      section: 'Features',
+    },
+    {
+      type: 'fix',
+      section: 'Bug Fixes',
+    },
+    {
+      type: 'perf',
+      section: 'Performance Improvements',
+    },
+    {
+      type: 'revert',
+      section: 'Reverts',
+    },
+    {
+      type: 'docs',
+      section: 'Documentation',
+    },
+    {
+      type: 'style',
+      section: 'Styles',
+    },
+    {
+      type: 'chore',
+      section: 'Miscellaneous Chores',
+    },
+    {
+      type: 'refactor',
+      section: 'Code Refactoring',
+    },
+    {
+      type: 'test',
+      section: 'Tests',
+    },
+    {
+      type: 'build',
+      section: 'Build System',
+    },
+    {
+      type: 'ci',
+      section: 'Continuous Integration',
+    },
+  ],
+});
+
+/**
+ *
+ * @param {string} version
+ * @param {boolean} onTag
+ * @returns
+ */
+export function getChangelog(version, onTag) {
+  return conventionalChangelogCore(
+    {
+      config,
+      releaseCount: onTag ? 2 : 1,
+    },
+    { version, linkCompare: false },
+    undefined,
+    undefined,
+    { headerPartial: '' },
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,80 @@
+import { Command, runExit } from 'clipanion';
+import { getChangelog } from './changelog/util.js';
+
+class GiteaReleaseCommand extends Command {
+  async execute() {
+    const url = new URL(process.env.GIT_URL);
+    const api = `${url.origin}/api/v1`;
+    const repo = url.pathname.replace(/\.git$/, '').replace(/^\//, '');
+    const token = process.env.GITEA_TOKEN;
+    const tag = process.env.TAG_NAME;
+
+    this.context.stdout.write(`Using api: ${api}.\n`);
+    this.context.stdout.write(`Using repository: ${repo}\n`);
+
+    if (!token) {
+      this.context.stdout.write('GITEA_TOKEN environment variable not set.\n');
+      return 1;
+    }
+
+    if (tag) {
+      this.context.stdout.write(`Using tag: ${tag}\n`);
+    } else {
+      this.context.stdout.write('No tag found for this commit. Please tag the commit and try again.\n');
+      return 1;
+    }
+
+    this.context.stdout.write(`Checking remote tag ${tag}.\n`);
+    let resp = await fetch(`${api}/repos/${repo}/tags/${tag}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!resp.ok) {
+      this.context.stdout.write(`Tag ${tag} not found on remote.\n`);
+      return 1;
+    }
+
+    this.context.stdout.write(`Checking remote release ${tag}.\n`);
+    resp = await fetch(`${api}/repos/${repo}/releases/tags/${tag}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (resp.ok) {
+      this.context.stdout.write(`Release ${tag} already exists.\n`);
+      return 1;
+    } else if (resp.status !== 404) {
+      this.context.stdout.write(`Error checking for release ${tag}.\n${resp.status}: ${resp.statusText}\n`);
+      return 1;
+    }
+
+    const stream = getChangelog(tag, true).setEncoding('utf8');
+    const changes = (await stream.toArray()).join('');
+
+    this.context.stdout.write(`Creating release ${tag}.\n`);
+    resp = await fetch(`${api}/repos/${repo}/releases`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        draft: false,
+        prerelease: tag.includes('-'),
+        tag_name: tag,
+        name: tag.replace(/^v/, ''),
+        body: changes,
+        target_commitish: 'main',
+      }),
+    });
+
+    if (!resp.ok) {
+      this.context.stdout.write(`Error creating release ${tag}.\n${resp.status}: ${resp.statusText}\n`);
+      return 1;
+    }
+  }
+}
+
+void runExit(GiteaReleaseCommand);
